dd17531d9723a85c9f3b5f164d996d9d63ec276b
Juan Carlos Londoño <juan.londono11@udea.edu.co>
2020-03-18 20:29:19 -0500

    .

--- src/main/java/com/udea/iotProject/broker/IotSender.java
+++ src/main/java/com/udea/iotProject/broker/IotSender.java
@@ -11,8 +11,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class IotSender {
 
-    @Autowired
-    private RabbitTemplate template;
+    RabbitTemplate template;
 
 
     @Scheduled(fixedDelay = 1000, initialDelay = 500)
